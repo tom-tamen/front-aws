1b89a3acdd66f0782625f5da157312a4069bab0c
Tom.T <tomtamen@gmx.fr>
2025-07-22 09:44:23 +0200

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,7 +7,7 @@ describe('Compteur', () => {
   it('devrait incrémenter le compteur quand on clique sur le bouton', () => {
     render(<App />)
     const button = screen.getByRole('button', { name: /count is/i })
-    expect(button).toHaveTextContent('count is failed')
+    expect(button).toHaveTextContent('count is 0')
     fireEvent.click(button)
     expect(button).toHaveTextContent('count is 1')
     fireEvent.click(button)
